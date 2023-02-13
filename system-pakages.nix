cebf8f6aab01fa8d8a156139a4a8a84753072358
Oliver Sargison <sargo@sargo.cc>
2023-02-14 07:06:12 +1300

    made some modules

---
+++ system-pakages.nix
@@ -0,0 +1,39 @@
+{ config, pkgs, lib, ... }: {
+
+  environment.systemPackages = with pkgs; [
+    catppuccin-gtk
+    gcc
+    any-nix-shell
+    gnome.dconf-editor
+    waybar
+    pulsemixer
+    pavucontrol
+    pulseaudio
+    nodePackages_latest.typescript-language-server
+    nodejs
+    font-awesome 
+    jetbrains-mono
+    unstable.gnomeExtensions.custom-accent-colors
+    unstable.gnomeExtensions.fuzzy-app-search
+    unstable.gnomeExtensions.caffeine
+    unstable.gnomeExtensions.dash-to-dock
+    unstable.gnomeExtensions.blur-my-shell
+    unstable.gnomeExtensions.uptime-indicator
+    unstable.gnomeExtensions.user-themes
+    unstable.gnome.gnome-tweaks
+    unstable.gnome.nautilus
+    unstable.gnome.gnome-themes-extra
+    unstable.gtk-engine-murrine
+    xdg-desktop-portal
+    xdg-desktop-portal-wlr
+    hyprland
+    xdg-desktop-portal-gtk
+    unstable.helix
+    unstable.firefox
+    gcc
+    pkg-config
+    unstable.fontconfig
+    unstable.freetype
+    openssl
+  ];
+}
