0d965167c8f2f8c2b0228a95b66a2643209b1fe2
Oliver Sargison <sargo@sargo.cc>
2023-03-10 18:55:22 +1300

    Refactor

--- configuration.nix
+++ configuration.nix
@@ -136,20 +136,20 @@ let
       hyprland.nixosModules.default
     ];
   nixpkgs.config.allowUnfree = true;
-  i18n.defaultLocale = "en_NZ.UTF-8";
   time.timeZone = "Pacific/Auckland";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_NZ.UTF-8";
-    LC_IDENTIFICATION = "en_NZ.UTF-8";
-    LC_MEASUREMENT = "en_NZ.UTF-8";
-    LC_MONETARY = "en_NZ.UTF-8";
-    LC_NAME = "en_NZ.UTF-8";
-    LC_NUMERIC = "en_NZ.UTF-8";
-    LC_PAPER = "en_NZ.UTF-8";
-    LC_TELEPHONE = "en_NZ.UTF-8";
-    LC_TIME = "en_NZ.UTF-8";
+  i18n = {
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_NZ.UTF-8";
+      LC_IDENTIFICATION = "en_NZ.UTF-8";
+      LC_MEASUREMENT = "en_NZ.UTF-8";
+      LC_MONETARY = "en_NZ.UTF-8";
+      LC_NAME = "en_NZ.UTF-8";
+      LC_NUMERIC = "en_NZ.UTF-8";
+      LC_PAPER = "en_NZ.UTF-8";
+      LC_TELEPHONE = "en_NZ.UTF-8";
+      LC_TIME = "en_NZ.UTF-8";
+    };
   };
-  programs.wshowkeys.enable = true;
-  services.systembus-notify.enable = true;
+  
   system.autoUpgrade.enable = true;
 }
