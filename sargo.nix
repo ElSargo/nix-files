03fc9acb48871f900e2defab80fc93470103a98a
Oliver Sargison <sargo@sargo.cc>
2023-05-04 07:35:13 +1200

    Sorted palette

--- sargo.nix
+++ sargo.nix
@@ -66,34 +66,34 @@
       }).defaultNix;
 
       palette = {
+        aqua = "#689d6a";
         bg = "#282828"; # main background
-        br_bg = "#3c3836";
         bg2 = "#504945";
         bg3 = "#665c54";
         bg4 = "#7c6f64";
-        fg = "#fbf1c7";
+        blue = "#458588";
+        br_aqua = "#8ec07c";
+        br_bg = "#3c3836";
+        br_blue = "#83a598";
         br_fg = "#ebdbb2"; # main foreground
+        br_gray = "#928374";
+        br_green = "#b8bb26";
+        br_orange = "#fe8019";
+        br_purple = "#d3869b";
+        br_red = "#fb4934";
+        br_yellow = "#fabd2f";
+        fg = "#fbf1c7";
         fg2 = "#d5c4a1";
         fg3 = "#bdae93";
         fg4 = "#a89984"; # gray0
         gray = "#a89984";
-        br_gray = "#928374";
-        red = "#cc241d";
-        br_red = "#fb4934"; # bright
         green = "#98971a";
-        br_green = "#b8bb26";
-        yellow = "#d79921";
-        br_yellow = "#fabd2f";
-        blue = "#458588";
-        br_blue = "#83a598";
-        purple = "#b16286";
-        br_purple = "#d3869b";
-        aqua = "#689d6a";
-        br_aqua = "#8ec07c";
         orange = "#d65d0e";
-        br_orange = "#fe8019";
-        white = "#fbf1c7";
+        purple = "#b16286";
+        red = "#cc241d";
         tan = "#bdae93";
+        white = "#fbf1c7";
+        yellow = "#d79921";
       };
 
     in {
