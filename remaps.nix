f1bb9ed0b87c0fba7f2ee7f38ece0ba5370c6ff3
Oliver Sargison <sargo@sargo.cc>
2023-05-19 20:19:38 +1200

    Add remaps

---
+++ remaps.nix
@@ -0,0 +1,22 @@
+{ pkgs, ... }: {
+  environment.etc."dual-function-keys.yaml".text = ''
+    MAPPINGS:
+      - KEY: KEY_CAPSLOCK
+        TAP: KEY_ESC
+        HOLD: KEY_LEFTMETA
+      - KEY: KEY_TAB
+        TAP: KEY_TAB
+        HOLD: KEY_LEFTCTRL
+  '';
+  services.interception-tools = {
+    enable = true;
+    plugins = [ pkgs.interception-tools-plugins.dual-function-keys ];
+    udevmonConfig = ''
+      - JOB: "${pkgs.interception-tools}/bin/intercept -g $DEVNODE | ${pkgs.interception-tools-plugins.dual-function-keys}/bin/dual-function-keys -c /etc/dual-function-keys.yaml | ${pkgs.interception-tools}/bin/uinput -d $DEVNODE"
+        DEVICE:
+          EVENTS:
+            EV_KEY: [KEY_CAPSLOCK, KEY_ESC, KEY_TAB ]
+    '';
+  };
+
+}
