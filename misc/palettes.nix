2f067d30a1dde78be998025542028117202eac39
Oliver Sargison <sargo@sargo.cc>
2023-07-16 09:43:39 +1200

    Moved palettes into own file

---
+++ misc/palettes.nix
@@ -0,0 +1,65 @@
+{
+  gruvbox = {
+    helix_theme = "gruvy";
+    aqua = "#689d6a";
+    bg = "#282828"; # main background
+    bg2 = "#504945";
+    bg3 = "#665c54";
+    bg4 = "#7c6f64";
+    blue = "#458588";
+    br_aqua = "#8ec07c";
+    br_bg = "#3c3836";
+    br_blue = "#83a598";
+    br_fg = "#ebdbb2"; # main foreground
+    br_gray = "#928374";
+    br_green = "#b8bb26";
+    br_orange = "#fe8019";
+    br_purple = "#d3869b";
+    br_red = "#fb4934";
+    br_yellow = "#fabd2f";
+    fg = "#fbf1c7";
+    fg2 = "#d5c4a1";
+    fg3 = "#bdae93";
+    fg4 = "#a89984"; # gray0
+    gray = "#a89984";
+    green = "#98971a";
+    orange = "#d65d0e";
+    purple = "#b16286";
+    red = "#cc241d";
+    tan = "#bdae93";
+    white = "#fbf1c7";
+    yellow = "#d79921";
+  };
+  tokionight = rec {
+    helix_theme = "tokio";
+    aqua = "#89ddff";
+    bg = "#16161e"; # main background
+    bg2 = "#373d5a";
+    bg3 = br_bg;
+    bg4 = bg3;
+    blue = "#7aa2f7";
+    br_aqua = "#b4f9f8";
+    br_bg = "#1f2335";
+    br_blue = "#2ac3de";
+    br_gray = "#9aa5ce";
+    br_green = "#9ece6a";
+    br_orange = br_aqua;
+    br_purple = "#bb9af7";
+    br_red = red;
+    br_yellow = yellow;
+    fg = "#a9b1d6";
+    br_fg = "#c0caf5"; # main foreground
+    fg2 = "#363b54";
+    fg3 = br_fg;
+    fg4 = br_blue; # gray0
+    gray = br_gray;
+    green = "#73daca";
+    orange = "#ff9e64";
+    purple = "#7dcfff";
+    red = "#f7768e";
+    tan = blue;
+    white = "#c0caf5";
+    yellow = "#e0af68";
+  };
+}
+
