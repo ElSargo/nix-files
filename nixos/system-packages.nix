944d54a53168a9a029b183295104b0835b107f0a
Oliver Sargison <sargo@sargo.cc>
2023-07-30 07:13:14 +1200

    Removed redundant deps

--- nixos/system-packages.nix
+++ nixos/system-packages.nix
@@ -5,7 +5,6 @@ let
     pulsemixer
     pavucontrol
     pulseaudio
-    unstable.ncspot
     unstable.psst
     unstable.qpwgraph
     unstable.jamesdsp
@@ -13,17 +12,11 @@ let
 
   gnome = with pkgs; [
     gnomeExtensions.removable-drive-menu
-    gnomeExtensions.custom-accent-colors
     gnomeExtensions.caffeine
     gnomeExtensions.dash-to-dock
     gnomeExtensions.blur-my-shell
     gnomeExtensions.uptime-indicator
-    gnomeExtensions.user-themes
-    gnomeExtensions.gsconnect
     gnomeExtensions.grand-theft-focus
-    # gnome.gnome-tweaks
-    # gnome.gnome-themes-extra
-
   ];
 
   networking = with pkgs; [ blueberry blueman ];
@@ -66,18 +59,8 @@ let
     cargo
   ];
 
-  graphics = with pkgs; [ blender ];
 
-  gtk = with pkgs; [
-    unstable.gtk-engine-murrine
-    gruvbox-dark-gtk
-    gruvbox-dark-icons-gtk
-    catppuccin-gtk
-  ];
-
-  fonts = with pkgs; [ fontconfig freetype ];
-
-  desktop = with pkgs; [ unstable.thunderbird glava inlyne swaybg feh libreoffice ];
+  desktop = with pkgs; [ inlyne swaybg feh libreoffice ];
 
   system = with pkgs; [ cpupower-gui ];
 
@@ -101,13 +84,10 @@ in [
   utils
   coding
   terminal
-  gtk
-  fonts
   desktop
   compat
   browsers
   system
-  graphics
   custom
 ]
 
